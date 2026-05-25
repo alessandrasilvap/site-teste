eff87ae5a426d0a6ce658c32bfe7df5bd9022cc5
Alessandra Pereira <alessandracris393@gmail.com>
2026-05-25 09:00:09 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,1 +1,12 @@
+<?php
+require_once __DIR__ . '/../app/controllers/UsuarioController.php';
 
+$controller = new UsuarioController();
+
+$acao = $_GET['acao'] ?? 'index';
+
+if ($acao == 'criar') {
+    $controller->criar();
+} else {
+    $controller->index();
+}
