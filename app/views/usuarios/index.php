88489bc302569a44d27b99d50c258842c1bfbda7
Alessandra Pereira <alessandracris393@gmail.com>
2026-05-25 08:59:38 -0300

    Add user list view in index.php

--- app/views/usuarios/index.php
+++ app/views/usuarios/index.php
@@ -1,1 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
 
+<h1>Usuários</h1>
+
+<a href="/?acao=criar">Novo usuário</a>
+
+<ul>
+<?php foreach ($usuarios as $u): ?>
+    <li><?= $u['nome'] ?> - <?= $u['email'] ?></li>
+<?php endforeach; ?>
+</ul>
+
+</body>
+</html>
