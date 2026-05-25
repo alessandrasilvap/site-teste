22ca4ddbf71214bb30ae8b735634097f9fe94dbe
Alessandra Pereira <alessandracris393@gmail.com>
2026-05-25 08:58:38 -0300

    Add Database class with connection method

--- app/config/Database.php
+++ app/config/Database.php
@@ -1,1 +1,11 @@
+<?php
 
+class Database {
+    public static function conectar() {
+        return new PDO(
+            "mysql:host=localhost;dbname=mvc_exemplo",
+            "root",
+            ""
+        );
+    }
+}
