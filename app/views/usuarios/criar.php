18ad3fdb5bfd4aeddd2dcc2693437733d6e329e9
Alessandra Pereira <alessandracris393@gmail.com>
2026-05-25 08:59:54 -0300

    Add user creation form in criar.php

--- app/views/usuarios/criar.php
+++ app/views/usuarios/criar.php
@@ -1,1 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
 
+<h1>Novo Usuário</h1>
+
+<form method="POST">
+    <input name="nome" placeholder="Nome">
+    <input name="email" placeholder="Email">
+    <button>Salvar</button>
+</form>
+
+</body>
+</html>
