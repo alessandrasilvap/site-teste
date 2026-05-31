2d08bf86a1539731e10e0f80029f6211dffbfc7d
Alessandra Pereira <alessandracris393@gmail.com>
2026-05-31 14:31:42 -0300

    Refactor user index view layout and styles
    Updated the stylesheet link and wrapped user list in a container.

--- app/views/usuarios/index.php
+++ app/views/usuarios/index.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="../public/css/style.css">
 </head>
 <body>
     <!--Exibição da lista de usuários-->
-    <h1>Usuários</h1>
+    <div class="container">
+        <h1>Usuários</h1>
 
-    <a href="/site-teste/public/?acao=criar">Novo usuário</a>
+        <a href="/site-teste/public/?acao=criar">Novo usuário</a>
 
-    <ul>
-    <?php foreach ($usuarios as $u): ?>
-        <li><?= $u['nome'] ?> - <?= $u['email'] ?></li>
-    <?php endforeach; ?>
-    </ul>
+        <ul>
+            <?php foreach ($usuarios as $u): ?>
+                <li><?= $u['nome'] ?> - <?= $u['email'] ?></li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
 </body>
 </html>
